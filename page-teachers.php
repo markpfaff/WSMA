769941869c5f7c9b059b7889a07272b513f6ca4f
markpfaff <markdpfaff@gmail.com>
2015-12-08 17:17:39 -0800

    Fixes teacher image on class page, adds page-teachers, mobile styles

--- page-teachers.php
+++ page-teachers.php
@@ -1,9 +1,5 @@
-<?php
-/*
-Template Name: Individual Class Page
-*/
-?>
 <?php get_header(); ?>
+
 
 
 <!-- content start -->
@@ -13,30 +9,29 @@ Template Name: Individual Class Page
         <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
         <h1 class="col-md-8"><?php the_title(); ?></h1>
-
         <article class="page page-intro-p" id="post-<?php the_ID(); ?>">
             <div class="entry">
             <?php the_content('<p class="serif">More &raquo;</p>'); ?>
         	</div>
-            <div class="page-buttons">
-                <a href="<?php echo get_page_link( get_page_by_title( 'enroll' )->ID ); ?>">
-                    <button type="button" class="btn btn-info btn-lg">Enroll</button>
-                </a>
-                <a href="<?php echo get_page_link( get_page_by_title( 'policies' )->ID ); ?>">
-                    <button type="button" class="btn btn-info btn-lg">Policies</button>
-                </a>
+        </article>
+        <?php endwhile; else:?>
+        <article class="page col-md-9 not-found">
+            <div class="entry">
+                <p class="lead"><?php _e('Sorry, this page does not exist. Try searching for one.'); ?></p>
+                <?php get_search_form(); ?>
             </div>
         </article>
-            <!-- sidebar -->
+        <?php endif; ?>
+                        	<!-- sidebar -->
             <?php get_sidebar( 'primary' ); ?>
             <!-- /sidebar -->
         <div class="classes-teacher-container">
                 <h2>Teachers</h2>
-                <?php $thumbquery = new WP_Query(array( 'classes' => 'bass', 'post_type' => 'teachers' ));
+                <?php $thumbquery = new WP_Query(array('post_type' => 'teachers' ));
                            while ( $thumbquery->have_posts() ) : $thumbquery->the_post(); 
                            ?>
                 
-                <div class="col-xs-3">
+                <div class="teachers-bio-container">
 
                     <a href="<?php the_permalink; ?>">
                         <?php  //get_post_thumbnail_id('medium');
@@ -45,26 +40,15 @@ Template Name: Individual Class Page
  
                     </a>
                     <caption><?php echo the_excerpt(); ?></caption>
+                    <?php the_content(); ?>
                     
                 </div>
                 <?php endwhile; wp_reset_postdata(); ?> 
         </div>
-    </div><!--/row-->
-</div><!--/container-->   
-        
-        <?php endwhile; else:?>
-        <article class="classes-teacher-container not-found">
-            <div class="entry">
-                <p class="lead"><?php _e('Sorry, this page does not exist. Try searching for one.'); ?></p>
-                <?php get_search_form(); ?>
-            </div>
-        </article>
-        <?php endif; ?>
-        
 
+    </div><!-- /row -->
 </div><!-- /container -->
 
-<small class="small-label">page-classes1.php</small>
+<small class="small-label">page.php</small>
 
 <?php get_footer(); ?>
-
